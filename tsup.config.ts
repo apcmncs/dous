27dcc8ee2543a92ea85693a491fb36597c4d7d6f
apcmncs <apcmncs@gmail.com>
2025-06-16 09:55:35 +0700

    fix: remove commented-out configuration in tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,5 @@
 import { defineConfig } from "tsup";
 
-// export default defineConfig({
-// 	entry: ["src/index.ts"],
-// 	format: ["cjs", "esm"],
-// 	dts: true,
-// 	minify: true,
-// 	splitting: false,
-// 	sourcemap: true,
-// 	clean: true,
-// });
-
 export default defineConfig((options) => {
 	return {
 		entry: ["src/index.ts"],
